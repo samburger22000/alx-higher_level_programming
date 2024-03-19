8a263d45b87f49afc3819be7e053253120d5aea4
samburger22000 <samburger22000@gmail.com>
2024-03-18 22:01:43 -0700

    Modification of list

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,7 @@
-#include <stddef.h>
 #ifndef LISTS_H
 #define LISTS_H
+
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -8,12 +9,12 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for project
+ * for Holberton project
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -23,4 +24,3 @@ void free_listint(listint_t *head);
 int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */
-
